267e1a3a0b44a61a8eab5290c83cadc402061110
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-26 08:59:45 -0500

    change to own approach

--- src/pages/r3f/terrain.tsx
+++ src/pages/r3f/terrain.tsx
@@ -1,9 +1,21 @@
 import { WorldManager } from "@components/canvas/ChunkGenerationSystem/WorldManager";
+import { MinecraftCreativeControlsPlayer } from "@components/canvas/FlyingPlayer";
+import { KeyboardControlsProvider } from "@components/canvas/Scene";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
 
 const Page = () => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <WorldManager />
+      <KeyboardControlsProvider>
+        <Canvas>
+          <Physics>
+            <ambientLight intensity={0.5} />
+            <WorldManager />
+            <MinecraftCreativeControlsPlayer speed={20} />
+          </Physics>
+        </Canvas>
+      </KeyboardControlsProvider>
     </div>
   );
 };

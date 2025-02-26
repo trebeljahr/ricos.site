2474942692bcbdc4fc2620142b0ff69d410b7d91
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-26 17:01:51 -0500

    remove controls context again

--- src/pages/r3f/terrain.tsx
+++ src/pages/r3f/terrain.tsx
@@ -1,36 +1,29 @@
-import {
-  ControlsContextProvider,
-  useControlsContext,
-} from "@components/canvas/ChunkGenerationSystem/ControlsContextProvider";
 import { WorldManager } from "@components/canvas/ChunkGenerationSystem/WorldManager";
 import { MinecraftCreativeControlsPlayer } from "@components/canvas/FlyingPlayer";
 import { KeyboardControlsProvider } from "@components/canvas/Scene";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
+import { useControls } from "leva";
 
-const MyCanvas = () => {
-  const { speed } = useControlsContext();
-
-  return (
-    <Canvas>
-      <Physics>
-        <ambientLight intensity={1.0} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <fogExp2 attach="fog" args={["#f0f0f0", 0.002]} />
-        <color args={["#f0f0f0"]} attach="background" />
-        <WorldManager />
-        <MinecraftCreativeControlsPlayer speed={speed} />
-      </Physics>
-    </Canvas>
-  );
-};
+const defaultSpeed = 25;
 const Page = () => {
+  const { speed } = useControls({
+    speed: { value: defaultSpeed, min: 1, max: 50, step: 1 },
+  });
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <KeyboardControlsProvider>
-        <ControlsContextProvider>
-          <MyCanvas />
-        </ControlsContextProvider>
+        <Canvas>
+          <Physics>
+            <ambientLight intensity={1.0} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <fogExp2 attach="fog" args={["#f0f0f0", 0.002]} />
+            <color args={["#f0f0f0"]} attach="background" />
+            <WorldManager />
+            <MinecraftCreativeControlsPlayer speed={speed} />
+          </Physics>
+        </Canvas>
       </KeyboardControlsProvider>
     </div>
   );

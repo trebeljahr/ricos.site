58148c2cbb0f51dfe9b7bd7af7a9691025668b55
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-27 17:22:37 -0500

    small fixes

--- src/pages/r3f/allModels.tsx
+++ src/pages/r3f/allModels.tsx
@@ -8,17 +8,35 @@ import { Physics } from "@react-three/rapier";
 import { Leva } from "leva";
 import * as assets from "@models/simple_nature_pack";
 import * as moreAssets from "@models/nature_pack";
-import { Html } from "@react-three/drei";
+import { Html, Plane } from "@react-three/drei";
+import { DoubleSide } from "three";
 
 const Page = () => {
+  const groundColor = "#84fb34";
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div className="w-screen h-screen bg-white">
       <KeyboardControlsProvider>
         <Leva />
         <Canvas>
           <Physics debug={physicsDebug}>
             <ambientLight intensity={1.0} />
             <directionalLight position={[10, 10, 5]} intensity={1} />
+            <hemisphereLight
+              color={"#FFFFFF"}
+              position={[0, 50, 0]}
+              groundColor={groundColor}
+            />
+            <Plane
+              args={[100, 100]}
+              rotation={[-Math.PI / 2, 0, 0]}
+              position={[40, 0, 0]}
+            >
+              <meshStandardMaterial color={groundColor} side={DoubleSide} />
+            </Plane>
+            {/* // const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2
+            // ); hemiLight.color.setHSL( 0.6, 1, 0.6 );
+            // hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+            // hemiLight.position.set( 0, 50, 0 ); */}
             <fogExp2 attach="fog" args={["#f0f0f0", 0.002]} />
             <color args={["#f0f0f0"]} attach="background" />
             {Object.entries(assets).map(([key, Asset], index) => {
@@ -28,11 +46,13 @@ const Page = () => {
                 <group key={index} position={[index * 3, 0, -10]}>
                   <Html
                     center
-                    position={[0, -0.5, 0]}
+                    position={[0, 0.5, -2]}
                     scale={[-1, 1, 1]}
                     transform
+                    occlude="blending"
+                    className="text-black bg-white p-2"
                   >
-                    <div style={{ color: "black" }}>{key}</div>
+                    {key}
                   </Html>
                   <Asset />
                 </group>
@@ -48,11 +68,13 @@ const Page = () => {
                 >
                   <Html
                     center
-                    position={[0, -0.5, 0]}
+                    position={[0, 0.5, -2]}
                     scale={[-1, 1, 1]}
                     transform
+                    occlude="blending"
+                    className="text-black bg-white p-2"
                   >
-                    <div style={{ color: "black" }}>{key}</div>
+                    {key}
                   </Html>
                   <Asset />
                 </group>

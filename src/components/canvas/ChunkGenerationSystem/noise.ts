b7f5822f16f12f2b686d68e28b02b130707bfa75
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-27 15:51:11 -0500

    refactor biomes to trees map

--- src/components/canvas/ChunkGenerationSystem/noise.ts
+++ src/components/canvas/ChunkGenerationSystem/noise.ts
@@ -7,6 +7,7 @@ import {
   temperatureNoiseScale,
 } from "./config";
 import { Color } from "three";
+import { TreeType } from "./TreeSystem";
 
 const normalizeNoise = (func: NoiseFunction2D) => {
   return (x: number, y: number) => (func(x, y) + 1) / 2;
@@ -53,9 +54,9 @@ export type BiomeName =
   | "Beach"
   | "Desert"
   | "Savanna"
-  | "Tropical Forest"
+  | "Jungle"
   | "Forest"
-  | "Dense Forest"
+  | "DenseForest"
   | "Plains"
   | "Tundra"
   | "Taiga";
@@ -64,99 +65,136 @@ export type BiomeType = {
   color: Color;
   name: BiomeName;
   treeDensity: number;
+  treeTypes: TreeType[];
+};
+
+const Biomes: Record<BiomeName, BiomeType> = {
+  Snow: {
+    name: "Snow",
+    color: new Color("#FFFFFF"),
+    treeDensity: 0.1,
+    treeTypes: [TreeType.PINE],
+  },
+  Mountain: {
+    name: "Mountain",
+    color: new Color("#A0A0A0"),
+    treeDensity: 0.4,
+    treeTypes: [TreeType.PINE, TreeType.SPRUCE],
+  },
+  Ocean: {
+    name: "Ocean",
+    color: new Color("#0077BE"),
+    treeDensity: 0,
+    treeTypes: [],
+  },
+
+  Beach: {
+    name: "Beach",
+    color: new Color("#C2B280"),
+    treeDensity: 0.15,
+    treeTypes: [TreeType.PALM],
+  },
+
+  Desert: {
+    name: "Desert",
+    color: new Color("#EDC9AF"),
+    treeDensity: 0.05,
+    treeTypes: [TreeType.ACACIA],
+  },
+
+  Savanna: {
+    name: "Savanna",
+    color: new Color("#ADFF2F"),
+    treeDensity: 0.3,
+    treeTypes: [TreeType.ACACIA],
+  },
+
+  Jungle: {
+    name: "Jungle",
+    color: new Color("#228B22"),
+    treeDensity: 0.8,
+    treeTypes: [TreeType.PALM, TreeType.JUNGLE],
+  },
+
+  Forest: {
+    name: "Forest",
+    color: new Color("#228B22"),
+    treeDensity: 0.7,
+    treeTypes: [TreeType.OAK, TreeType.PINE],
+  },
+
+  DenseForest: {
+    name: "DenseForest",
+    color: new Color("#006400"),
+    treeDensity: 0.7,
+    treeTypes: [TreeType.OAK, TreeType.PINE, TreeType.JUNGLE],
+  },
+
+  Plains: {
+    name: "Plains",
+    color: new Color("#b2da20"),
+    treeDensity: 0.3,
+    treeTypes: [TreeType.OAK, TreeType.ACACIA],
+  },
+
+  Tundra: {
+    name: "Tundra",
+    color: new Color("#B5B5B5"),
+    treeDensity: 0.1,
+    treeTypes: [TreeType.PINE],
+  },
+
+  Taiga: {
+    name: "Taiga",
+    color: new Color("#9de19d"),
+    treeDensity: 0.6,
+    treeTypes: [TreeType.PINE, TreeType.SPRUCE],
+  },
 };
 
 function getBiome(nTemp: number, nMoist: number, nHeight: number): BiomeType {
   if (nHeight > 0.7) {
-    return {
-      color: new Color("#FFFFFF"),
-      name: "Snow",
-      treeDensity: 0.1,
-    };
+    return Biomes.Snow;
   }
 
   if (nHeight > 0.6) {
-    return {
-      color: new Color("#A0A0A0"),
-      name: "Mountain",
-      treeDensity: 0.4,
-    };
+    return Biomes.Mountain;
   }
 
   if (nHeight < 0.3) {
     if (nMoist > 0.6) {
-      return {
-        color: new Color("#0077BE"),
-        name: "Ocean",
-        treeDensity: 0,
-      };
+      return Biomes.Ocean;
     }
     if (nMoist > 0.3) {
-      return {
-        color: new Color("#C2B280"),
-        name: "Beach",
-        treeDensity: 0.15,
-      };
+      return Biomes.Beach;
     }
   }
 
   if (nTemp > 0.6) {
     if (nMoist < 0.3) {
-      return {
-        color: new Color("#EDC9AF"),
-        name: "Desert",
-        treeDensity: 0.05,
-      };
+      return Biomes.Desert;
     }
     if (nMoist < 0.6) {
-      return {
-        color: new Color("#ADFF2F"),
-        name: "Savanna",
-        treeDensity: 0.3,
-      };
+      return Biomes.Savanna;
     }
-    return {
-      color: new Color("#228B22"),
-      name: "Tropical Forest",
-      treeDensity: 0.8,
-    };
+    return Biomes.Jungle;
   }
 
   if (nTemp > 0.3) {
     if (nMoist < 0.4) {
-      return {
-        color: new Color("#b2da20"),
-        name: "Plains",
-        treeDensity: 0.3,
-      };
+      return Biomes.Plains;
     }
     if (nMoist < 0.7) {
-      return {
-        color: new Color("#228B22"),
-        name: "Forest",
-        treeDensity: 0.7,
-      };
+      return Biomes.Forest;
     }
-    return {
-      color: new Color("#006400"),
-      name: "Dense Forest",
-      treeDensity: 0.7,
-    };
+    return Biomes.DenseForest;
   }
 
   if (nMoist < 0.4) {
-    return {
-      color: new Color("#B5B5B5"),
-      name: "Tundra",
-      treeDensity: 0.1,
-    };
+    return Biomes.Tundra;
   }
 
-  return {
-    color: new Color("#9de19d"),
-    name: "Taiga",
-    treeDensity: 0.6,
-  };
+  return Biomes.Taiga;
 }
 
 export {

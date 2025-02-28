088883e1a0612d1e4ddbd43704bdf8c973230009
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-28 09:47:27 -0500

    try different strategy for rotating cone

--- src/components/canvas/Yuka/example.tsx
+++ src/components/canvas/Yuka/example.tsx
@@ -13,44 +13,28 @@ export function YukaSimulation() {
   const vehicle = useRef(new Vehicle());
   const target = useRef(new GameEntity());
 
-  const sync: RenderCallback<Mesh> = (entity, renderComponent) => {
-    // renderComponent.matrix.copy(entity.worldMatrix as unknown as Matrix4);
-    // renderComponent.applyMatrix4(renderComponent.matrix);
-
-    // renderComponent.setRotationFromQuaternion(
-    //   entity.rotation as unknown as Quaternion
-    // );
-
-    // const quaternion = new Quaternion(
-    //   entity.rotation.x,
-    //   entity.rotation.y,
-    //   entity.rotation.z,
-    //   entity.rotation.w
-    // );
-
-    // renderComponent.rotation.setFromQuaternion(quaternion);
-
-    // renderComponent.position.copy(entity.position as unknown as Vector3);
-
-    // renderComponent.applyQuaternion(entity.rotation);
-    // renderComponent.rotation.copy(entity.rotation as unknown as Euler);
-
-    renderComponent.matrix.copy(entity.worldMatrix as unknown as Matrix4);
-    renderComponent.matrix.decompose(
-      renderComponent.position,
-      renderComponent.quaternion,
-      renderComponent.scale
-    );
-  };
-
   const { camera } = useThree();
 
   useEffect(() => {
     if (vehicleMeshRef.current) {
-      vehicle.current.setRenderComponent(vehicleMeshRef.current, sync);
+      vehicle.current.setRenderComponent(vehicleMeshRef.current, (entity) => {
+        vehicleMeshRef.current.position.copy(
+          entity.position as unknown as Vector3
+        );
+        vehicleMeshRef.current.quaternion.copy(
+          entity.rotation as unknown as Quaternion
+        );
+      });
     }
     if (targetMeshRef.current) {
-      target.current.setRenderComponent(targetMeshRef.current, sync);
+      target.current.setRenderComponent(targetMeshRef.current, (entity) => {
+        targetMeshRef.current.position.copy(
+          entity.position as unknown as Vector3
+        );
+        targetMeshRef.current.quaternion.copy(
+          entity.rotation as unknown as Quaternion
+        );
+      });
     }
 
     entityManager.current.add(vehicle.current);
@@ -61,15 +45,15 @@ export function YukaSimulation() {
 
     const seekBehavior = new SeekBehavior(target.current.position);
     vehicle.current.steering.add(seekBehavior);
+    vehicle.current.maxSpeed = 0.1;
 
     const interval = setInterval(() => {
-      const x = Math.random() * 3;
-      const z = Math.random() * 3;
-      target.current.position.set(x, 0, z);
+      const x = Math.random() * 10;
+      const z = Math.random() * 10;
+      const y = 0; // Math.random() * 20;
 
-      console.log(vehicle.current.rotation);
-      console.log(vehicleMeshRef.current.rotation);
-    }, 5000);
+      target.current.position.set(x, y, z);
+    }, 3000);
 
     camera.lookAt(
       new Vector3(vehicle.current.position.x, 0, vehicle.current.position.z)
@@ -86,12 +70,16 @@ export function YukaSimulation() {
 
   return (
     <>
-      <mesh ref={vehicleMeshRef} matrixAutoUpdate={false}>
+      <mesh
+        ref={vehicleMeshRef}
+        matrixAutoUpdate={true}
+        rotation={[Math.PI * 0.5, 0, 0]}
+      >
         <coneGeometry args={[0.1, 0.5, 8]} />
         <meshNormalMaterial />
       </mesh>
 
-      <mesh ref={targetMeshRef} matrixAutoUpdate={false}>
+      <mesh ref={targetMeshRef} matrixAutoUpdate={true}>
         <sphereGeometry args={[0.1, 32, 32]} />
         <meshPhongMaterial color={0xffea00} />
       </mesh>

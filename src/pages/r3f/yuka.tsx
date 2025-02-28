8a2584296f1eb9a8825552c2e0e7c53937755658
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-28 08:58:24 -0500

    add yuka example

---
+++ src/pages/r3f/yuka.tsx
@@ -0,0 +1,26 @@
+import { MinecraftCreativeControlsPlayer } from "@components/canvas/FlyingPlayer";
+import { KeyboardControlsProvider } from "@components/canvas/Scene";
+import { YukaSimulation } from "@components/canvas/Yuka/example";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+
+const Page = () => {
+  return (
+    <div className="h-screen w-screen">
+      <KeyboardControlsProvider>
+        <Canvas>
+          <Physics>
+            <ambientLight intensity={1.0} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <fogExp2 attach="fog" args={["#f0f0f0", 0.002]} />
+            <color args={["#f0f0f0"]} attach="background" />
+            <MinecraftCreativeControlsPlayer speed={25} />
+            <YukaSimulation />
+          </Physics>
+        </Canvas>
+      </KeyboardControlsProvider>
+    </div>
+  );
+};
+
+export default Page;

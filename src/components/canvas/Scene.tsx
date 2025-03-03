904e7fe75be7c6457b1f89419730163f9821890c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-03-03 09:19:15 -0500

    add keymaps

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -8,23 +8,32 @@ export const farUnderwater = 50;
 export const farOverwater = 100;
 
 export const KeyboardControlsProvider = ({ children }: PropsWithChildren) => {
-  return (
-    <KeyboardControls
-      map={[
-        { name: "forward", keys: ["ArrowUp", "w", "W"] },
-        { name: "backward", keys: ["ArrowDown", "s", "S"] },
-        { name: "left", keys: ["ArrowLeft", "a", "A"] },
-        { name: "right", keys: ["ArrowRight", "d", "D"] },
-        { name: "jump", keys: ["Space"] },
-        { name: "descend", keys: ["c", "C"] },
-        { name: "sprint", keys: ["Shift"] },
-        { name: "attack", keys: ["F", "f"] },
-      ]}
-    >
-      {children}
-    </KeyboardControls>
-  );
+  const chars = "abcdefghijklmnopqrstuvwxyz".split("");
+  const numbers = "0123456789".split("");
+  const bigChars = chars.map((c) => c.toUpperCase());
+  const allChars = [...chars, ...bigChars];
+  // const allKeys = allChars.map((c) => `Key${c}`);
+  const maps = allChars.map((c) => ({
+    name: c,
+    keys: [c.toLowerCase(), c.toUpperCase()],
+  }));
+
+  const keyMaps = [
+    { name: "forward", keys: ["ArrowUp", "w", "W"] },
+    { name: "backward", keys: ["ArrowDown", "s", "S"] },
+    { name: "left", keys: ["ArrowLeft", "a", "A"] },
+    { name: "right", keys: ["ArrowRight", "d", "D"] },
+    { name: "jump", keys: ["Space"] },
+    { name: "descend", keys: ["c", "C"] },
+    { name: "sprint", keys: ["Shift"] },
+    { name: "attack", keys: ["F", "f"] },
+    ...maps,
+  ];
+  console.log(keyMaps);
+
+  return <KeyboardControls map={keyMaps}>{children}</KeyboardControls>;
 };
+
 export function CanvasWithControls({
   children,
   ...props

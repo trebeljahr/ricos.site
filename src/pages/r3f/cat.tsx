bbb3ff66d1b78b427bfb9cf4216c3282c2226df5
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-03-05 13:54:40 -0500

    fix things

---
+++ src/pages/r3f/cat.tsx
@@ -0,0 +1,19 @@
+import { ThreeFiberLayout } from "@components/dom/Layout";
+import { Cat } from "@models/cat";
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+export default function Page() {
+  return (
+    <ThreeFiberLayout>
+      <Canvas>
+        <color attach="background" args={["#ffcc32"]} />
+
+        <Stage>
+          <Cat />
+        </Stage>
+        <OrbitControls />
+      </Canvas>
+    </ThreeFiberLayout>
+  );
+}

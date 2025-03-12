e51c2ee76b880dc3c2ad17a7d5abb88831e2af10
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-03-12 14:35:51 -0500

    remove lods again

--- src/canvas/InstancedMeshSystem/useInstancedMesh2multiMaterial.tsx
+++ src/canvas/InstancedMeshSystem/useInstancedMesh2multiMaterial.tsx
@@ -1,20 +1,11 @@
 import { useGLTF } from "@react-three/drei";
 import { extend, Object3DNode, useThree } from "@react-three/fiber";
 import { InstancedMesh2 } from "@three.ez/instanced-mesh";
-import { memo, useCallback, useEffect, useMemo, useRef } from "react";
-import {
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  MeshLambertMaterial,
-  Object3D,
-  Vector3,
-} from "three";
-import { mergeBufferGeometries, mergeVertices } from "three-stdlib";
-import { GenericGltfResult } from "./GenericInstancingSystem";
+import { useEffect, useMemo, useRef } from "react";
 import { XYZ } from "src/@types";
-import { createSimplifiedGeometry } from "./createSimplifiedGeometry";
-import { tileSize } from "@r3f/ChunkGenerationSystem/config";
+import { BoxGeometry, Mesh, Object3D, Vector3 } from "three";
+import { mergeBufferGeometries } from "three-stdlib";
+import { GenericGltfResult } from "./GenericInstancingSystem";
 
 declare module "@react-three/fiber" {
   interface ThreeElements {
@@ -110,34 +101,6 @@ export const useInstancedMeshMultiMaterial = ({
       if (!ref.current) return;
 
       async function optimizeMesh() {
-        // const mergedGeo = mergeVertices(ref.current.geometry);
-        const mergedGeo = ref.current.geometry;
-        const lod1 = await createSimplifiedGeometry(mergedGeo, {
-          ratio: 0.07,
-          error: 0.2,
-          prune: true,
-        });
-        const lod2 = await createSimplifiedGeometry(mergedGeo, {
-          ratio: 0.05,
-          error: 0.2,
-          prune: true,
-        });
-        const lod3 = await createSimplifiedGeometry(mergedGeo, {
-          ratio: 0.03,
-          error: 0.2,
-          prune: true,
-        });
-        const lod4 = await createSimplifiedGeometry(mergedGeo, {
-          ratio: 0.02,
-          error: 0.2,
-          prune: true,
-        });
-
-        ref.current.addLOD(lod1, mergedMaterials, (tileSize / 2) * 1);
-        ref.current.addLOD(lod2, mergedMaterials, (tileSize / 2) * 2);
-        ref.current.addLOD(lod3, mergedMaterials, (tileSize / 2) * 3);
-        ref.current.addLOD(lod4, mergedMaterials, (tileSize / 2) * 4);
-
         ref.current.addShadowLOD(ref.current.geometry);
         ref.current.addShadowLOD(new BoxGeometry(10, 20, 10), 5);
 

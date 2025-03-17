2cc43706cd30054536f09d6f7827efa6715cfd86
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-03-17 15:43:03 -0500

    working shaders for healthbars

--- src/pages/r3f/healthbars.tsx
+++ src/pages/r3f/healthbars.tsx
@@ -15,11 +15,51 @@ const Healthbars = () => {
     <>
       <HealthBar
         health={health}
+        position={[0, 3, 0]}
+        rotation={[0, 0, -Math.PI / 2]}
+        scale={[0.5, 1, 5]}
+        shape={Shapes.RHOMBUS}
+        waveAmp={0}
+        fillColor={"#ff0000"}
+        secondColor={"#1fb141"}
+        borderWidth={0.01}
+      />
+
+      <HealthBar
+        health={health}
+        position={[-3, 3, 0]}
+        rotation={[0, 0, 0]}
+        scale={[0.5, 1, 3]}
+        shape={Shapes.RHOMBUS}
+        waveAmp={0}
+        fillColor={"#f9ee27"}
+        borderWidth={0.03}
+      />
+
+      <HealthBar
+        health={health}
         position={[0, 1.5, 0]}
         rotation={[0, 0, -Math.PI / 2]}
         scale={[1, 1, 4]}
         shape={Shapes.BOX}
+        waveAmp={0}
+        borderColor={"#5f5e5e"}
+        borderWidth={0.02}
         fillColor={"#00e5ff"}
+      />
+
+      <HealthBar
+        health={health}
+        position={[4, -1, 0]}
+        rotation={[0, 0, 0]}
+        scale={[2, 1, 2]}
+        shape={Shapes.CIRCLE}
+        waveAmp={0.01}
+        waveFreq={32}
+        waveSpeed={0.3}
+        borderColor={"#363535"}
+        borderWidth={0.02}
+        fillColor={"#3279fd"}
       />
 
       <HealthBar
@@ -32,18 +72,42 @@ const Healthbars = () => {
 
       <HealthBar
         health={health}
-        position={[0, -1.5, 0]}
+        position={[-3, -1.5, 0]}
         scale={[2, 1, 1]}
+        bgColor={"#3a3a3a"}
         shape={Shapes.RHOMBUS}
         fillColor={"#1ddaa7"}
       />
 
       <HealthBar
         health={health}
-        position={[0, -3, 0]}
+        position={[0, -4.5, 0]}
         scale={[1, 1, 3]}
         shape={Shapes.CIRCLE}
+        borderWidth={0.01}
+        fillColor={"#ff397b"}
+      />
+
+      <HealthBar
+        health={health}
+        position={[3, -4, 0]}
+        rotation={[0, 0, -Math.PI / 2]}
+        waveAmp={0}
+        scale={[1, 1, 3]}
+        shape={Shapes.CIRCLE}
+        borderWidth={0.01}
         fillColor={"#f73b11"}
+      />
+
+      <HealthBar
+        health={health}
+        position={[-3, -4, 0]}
+        waveAmp={0}
+        scale={[2, 1, 2]}
+        bgColor={"#3a3a3a"}
+        borderColor={"#989898"}
+        shape={Shapes.CIRCLE}
+        fillColor={"#f7114e"}
       />
     </>
   );

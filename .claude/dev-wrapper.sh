4af48cd4ea3b8be221fc38afc38e59ba29da0daf
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2026-04-10 15:51:09 +0530

    Add dev-wrapper script to run next dev with node v22

---
+++ .claude/dev-wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH="/Users/rico/.nvm/versions/node/v22.20.0/bin:$PATH"
+export NODE="/Users/rico/.nvm/versions/node/v22.20.0/bin/node"
+exec /Users/rico/.nvm/versions/node/v22.20.0/bin/node ./node_modules/next/dist/bin/next dev -p 3456 --webpack
